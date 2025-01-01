a3bd8cee9cc65486cd0e9cc61b51137ad4ad26c1
Tushar <tusharmohan0416@gmail.com>
2025-01-02 00:16:31 +0530

    bugs fix

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -56,10 +56,10 @@ function Testimonials() {
                         {[...testimonials, ...testimonials].map((testimonial, index) => (
                             <div
                                 key={index}
-                                className='border border-white/15 p-6 md:p-10 rounded-lg bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-md flex-none'
+                                className='border border-white/15 p-4 md:p-10 rounded-lg bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-[200px] md:max-w-md flex-none text-center md:text-left'
                             >
-                                <div className='text-lg md:text-xl tracking-tight'>{testimonial.text}</div>
-                                <div className='flex items-center gap-3 mt-5'>
+                                <div className='text-sm md:text-xl tracking-tight'>{testimonial.text}</div>
+                                <div className='flex flex-col md:flex-row items-center gap-3 mt-5'>
                                     <div className='relative after:content-[""] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:rounded-lg after:mix-blend-soft-light before:absolute before:inset-0 before:content-[""] before:border before:border-white/20 before:z-10 before:rounded-lg'>
                                         <Image
                                             src={testimonial.avatarImg}
@@ -89,10 +89,10 @@ function Testimonials() {
                         {[...testimonials, ...testimonials].map((testimonial, index) => (
                             <div
                                 key={index}
-                                className='border border-white/15 p-6 md:p-10 rounded-lg bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-md flex-none'
+                                className='border border-white/15 p-4 md:p-10 rounded-lg bg-[linear-gradient(to_bottom_left,rgb(140,69,255,.3),black)] max-w-[200px] md:max-w-md flex-none text-center md:text-left'
                             >
-                                <div className='text-lg md:text-xl tracking-tight'>{testimonial.text}</div>
-                                <div className='flex items-center gap-3 mt-5'>
+                                <div className='text-sm md:text-xl tracking-tight'>{testimonial.text}</div>
+                                <div className='flex flex-col md:flex-row items-center gap-3 mt-5'>
                                     <div className='relative after:content-[""] after:absolute after:inset-0 after:bg-[rgb(140,69,244)] after:rounded-lg after:mix-blend-soft-light before:absolute before:inset-0 before:content-[""] before:border before:border-white/20 before:z-10 before:rounded-lg'>
                                         <Image
                                             src={testimonial.avatarImg}
